538e8723825001d5a1277473771245682d2a19b8
daniellichotti <danielclichotti@gmail.com>
2025-10-14 21:38:11 -0300

    sending arrays codes

--- ClassesEObjetos.js
+++ ClassesEObjetos.js
@@ -66,4 +66,90 @@ const newAnimal = new Dog('Pastor Alemão', '20.0', 'Grande')
 const newAnimal2 = new Cat('Siamês', '5.0', 'Pequeno')
 newAnimal2.makeSound()
 
+
+class Veiculo {
+    constructor(tipo, ano, marca, modelo) {
+        this.tipo = tipo
+        this.ano = ano
+        this.marca = marca
+        this.modelo = modelo
+    }
+
+    makeSound() {
+        console.log('Algum barulho de motor')
+    }
+}
+
+class Carro extends Veiculo {
+    constructor(ano, marca, modelo, motor, preco, cor) {
+        super('Carro', ano, marca, modelo)
+        this.motor = motor
+        this.preco = preco
+        this.cor = cor
+    }
+
+    makeSound() {
+        console.log('Vrummm')
+    }
+}
+
+class Aviao extends Veiculo {
+    constructor(ano, marca, modelo, motor, precoPassagem, combustivel) {
+        super('Aviao', ano, marca, modelo)
+        this.motor = motor
+        this.precoPassagem = precoPassagem
+        this.combustivel = combustivel
+    }
+
+    makeSound() {
+        console.log('Quack')
+    }
+}
+
+const newCar = new Carro(1999, 'Pegeout', '308', '1.0', 600.00, 'Verde Limão')
+const newPlane = new Aviao(1945, 'Embraer', 'Hercules', '1.2', 0, 'Querosene')
+console.log('O', newPlane.marca, newPlane.modelo, 'usa', newPlane.combustivel)
+console.log(newPlane)
+newPlane.makeSound()
+
 console.clear()
+
+class User {
+    #name
+    #email
+    #password
+
+    constructor(name, email, password) {
+        this.#name = name
+        this.#email = email
+        this.#password = password
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    set name(newName) {
+        if (newName.length <= 3) {
+            console.log('O nome deve ser maior que 3 caracteres')
+        } else {
+            this.#name = newName
+        }
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    set email(newEmail) {
+        if (!newEmail.includes("@")) {
+            console.log('O email deve conter @')
+        } else {
+            this.#email = newEmail
+        }
+    }
+}
+
+const newUser = new User('Moisés', 'moiseszinho@hotmail.com', '123456')
+newUser.email = 'leo'
+console.log(newUser.email)
