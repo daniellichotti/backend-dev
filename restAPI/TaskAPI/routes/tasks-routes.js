0912bbea1d5ca1ec4556afe6121b67a864e8c5f2
daniellichotti <danielclichotti@gmail.com>
2025-11-18 19:13:01 -0300

    sending prisma

--- restAPI/TaskAPI/routes/tasks-routes.js
+++ restAPI/TaskAPI/routes/tasks-routes.js
@@ -3,12 +3,12 @@ import { sendEmail } from "../utils/sendEmail.js"
 export async function taskRoutes(fastify, opts) {
 
     fastify.get('/', async (req, res) => {
-        await sendEmail(
-            'danielclichotti@gmail.com',
-            'Fala galera',
-            `<h1>Olá meu amigo</h1>
-            <p>Mensagem enviada com sucesso</p>`
-        )
+        // await sendEmail(
+        //     'danielclichotti@gmail.com',
+        //     'Fala galera',
+        //     `<h1>Olá meu amigo</h1>
+        //     <p>Mensagem enviada com sucesso</p>`
+        // )
         res.send('Lista de tarefas')
     })
 }
