7b3fcb77d9e62d1e69c44e03e45eccf198221d6c
daniellichotti <danielclichotti@gmail.com>
2025-11-18 21:32:06 -0300

    sending

--- restAPI/TaskAPI/routes/tasks-routes.js
+++ restAPI/TaskAPI/routes/tasks-routes.js
@@ -2,6 +2,18 @@ import { prisma } from '../lib/prisma.js'
 
 export async function taskRoutes(fastify, opts) {
 
+    const taskSchema = {
+        type: 'object',
+        required: ['title', 'description'],
+        properties: {
+            id: { type: 'id' },
+            title: { type: 'string' },
+            description: { type: 'string' },
+            createdAt: { type: 'string' },
+            completedAt: { type: 'string' },
+        }
+    }
+
     fastify.get('/', async (req, res) => {
         // await sendEmail(
         //     'danielclichotti@gmail.com',
@@ -14,7 +26,55 @@ export async function taskRoutes(fastify, opts) {
         res.send(tasks)
     })
 
-        .create({
+    fastify.post('/', {
+        schema: {
+            description: 'Cria uma nova Task',
+            tags: ['Task'],
+            body: {
+                type: 'object',
+                required: ['title', 'description'],
+                properties: {
+                    title: { type: 'string' },
+                    description: { type: 'string' }
+                }
+            }
+        },
+        response: {
+            201: taskSchema
+        }
+    }, async (req, res) => {
+        const { title, description } = req.body
+
+        const tasks = await prisma.task.create({
             data: { title, description }
         })
+
+        res.code(201).send(tasks)
+    })
+
+    fastify.delete('/:id', async (req, res) => {
+        const { id } = req.params
+
+        await prisma.task.delete({
+            where: { id: Number(id) }
+        })
+
+        res.code(200).send("Deletado com sucesso!")
+    })
+
+    fastify.patch('/:id', async (req, res) => {
+        const { id } = req.params
+
+
+        const task = await prisma.task.update({
+            where: { id: Number(id) },
+            data: {
+                completedAt: new Date()
+            }
+        })
+
+        res.code(200).send(task)
+    })
+
+
 }
